9d378c03f7000ee61a60745c97ee5490913eb8d8
Arief Hilmi <arief.hilmi@gmail.com>
2018-08-14 16:35:02 +0800

    upload codes
    upload codes

---
+++ bak.php
@@ -0,0 +1,80 @@
+<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('m3tech.com.my');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-building"></i>
+			<span class="heading">m3tech.com.my</span>
+			<span class="value"><span>M3 Tech</span></span>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('m3asia.com');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-credit-card"></i>
+			<span class="heading">m3asia.com</span>
+			<span class="value"><span>M3 Asia</span></span>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('m3online.com');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-desktop"></i>
+			<span class="heading">m3online.com</span>
+			<span class="value"><span>M3 Online</span></span>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('getsnapps.com');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-android"></i>
+			<span class="heading">getsnapps.com</span>
+			<span class="value"><span>GetSnapps</span></span>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('apps.m3tech.asia');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-apple"></i>
+			<span class="heading">apps.m3tech.asia</span>
+			<span class="value"><span>Apps M3 Tech</span></span>
+		</div>
+	</div>	
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('i3apps.com.my');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-mobile"></i>
+			<span class="heading">i3apps.com.my</span>
+			<span class="value"><span>i3 Apps</span></span>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('i3display.com');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-play-circle"></i>
+			<span class="heading">i3display.com</span>
+			<span class="value"><span>i3 Display</span></span>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-3 col-xs-6">
+		<?php 
+			$x = check_status('support.m3asia.com');			
+		?>
+		<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+			<i class="fa fa-child"></i>
+			<span class="heading">support.m3asia.com</span>
+			<span class="value"><span>Support M3 Asia</span></span>
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,286 @@
+<?php
+
+//require_once('vendor/autoload.php');
+//use Screen\Capture;
+set_time_limit(0);
+
+require 'vendor/autoload.php'; 
+use GuzzleHttp\Client;
+
+function check_status_with_heroku($http_code,$host)
+{
+	$disable_web_page_preview = null;
+	$reply_to_message_id = null;
+	$reply_markup = null;
+	//$chat_id = "676415365";
+	$chat_id = "449412519";
+	$smp_telegram_token = "676415365:AAFZWGH-kaUBpPR9xspIYz8n5MUA5AQ-EYs";
+	$url = "https://api.telegram.org/bot".$smp_telegram_token."/sendMessage";
+	
+	$offline_style = "background-color:#e84118 !important; border-radius:5px";
+	$online_style = "background-color:#4cd137 !important; border-radius:5px";
+	$exception_style = "background-color:#2980b9 !important; border-radius:5px";
+	
+	$heroku_url = "https://pw-url-checker.herokuapp.com/check?url=".$http_code.$host;
+	
+	$ch = curl_init($heroku_url);  
+    curl_setopt($ch, CURLOPT_TIMEOUT, 15);  
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $data = curl_exec($ch);	
+	$data = json_decode($data, true);	
+	$httpcode = $data['status'];
+	$httpmsg = $data['message'];
+	if($httpmsg == "")
+	{
+		$httpmsg = 'ok';
+	}
+	echo '<pre>'.$httpmsg.'</pre>';
+    curl_close($ch);  
+    if($httpcode>=200 && $httpcode<300)
+	{  
+        return $online_style;
+    }
+	else
+	{
+		$host_msg = "<b>".$host."</b>". " went offline";
+		$fields=array(
+						'chat_id' => $chat_id,
+						'parse_mode'=>'html',
+						'text' => "Oops, ".$host_msg,
+						'disable_web_page_preview' => urlencode($disable_web_page_preview),
+						'reply_to_message_id' => urlencode($reply_to_message_id),
+						'reply_markup' => urlencode($reply_markup)
+					);
+		//$url = "https://api.telegram.org/bot676415365:AAFZWGH-kaUBpPR9xspIYz8n5MUA5AQ-EYs/sendMessage?chat_id=449412519&text=test123456";
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, count($fields));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);	
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);		
+		curl_close($ch);
+        return $offline_style;
+    }
+}
+
+function check_status($host)
+{		
+	$disable_web_page_preview = null;
+	$reply_to_message_id = null;
+	$reply_markup = null;
+	//$chat_id = "676415365";
+	$chat_id = "449412519";
+	$smp_telegram_token = "676415365:AAFZWGH-kaUBpPR9xspIYz8n5MUA5AQ-EYs";
+	$url = "https://api.telegram.org/bot".$smp_telegram_token."/sendMessage";
+	
+	$offline_style = "background-color:#e84118 !important; border-radius:5px";
+	$online_style = "background-color:#4cd137 !important; border-radius:5px";
+	$exception_style = "background-color:#2980b9 !important; border-radius:5px";
+	    
+    $ch = curl_init($host);  
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);  
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $data = curl_exec($ch);  
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);	
+    curl_close($ch);  
+    if($httpcode>=200 && $httpcode<300)
+	{  
+        return $online_style;
+    }
+	else
+	{
+		$host_msg = "<b>".$host."</b>". " went offline";
+		$fields=array(
+						'chat_id' => $chat_id,
+						'parse_mode'=>'html',
+						'text' => "Oops, ".$host_msg,
+						'disable_web_page_preview' => urlencode($disable_web_page_preview),
+						'reply_to_message_id' => urlencode($reply_to_message_id),
+						'reply_markup' => urlencode($reply_markup)
+					);
+		//$url = "https://api.telegram.org/bot676415365:AAFZWGH-kaUBpPR9xspIYz8n5MUA5AQ-EYs/sendMessage?chat_id=449412519&text=test123456";
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, count($fields));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);	
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);		
+		curl_close($ch);
+        return $offline_style;
+    }
+}
+
+/* function get_screenshot($host)
+{
+	$url = 'http://'.$host;
+	$url = 'http://sayaarief.blogspot.my';
+	
+	$screenCapture = new Capture($url);	
+	$screenCapture->setImageType('png');	
+	
+	//$screenCapture->jobs->setLocation('img');
+	//echo $screenCapture->jobs->getLocation(); // -> /path/to/jobs/dir/
+	
+	$fileLocation = 'img';
+	echo "<pre>";
+	print_r($screenCapture);
+	echo "</pre>";
+	$screenCapture->save($fileLocation); // Will automatically determine the extension type			
+
+	echo $screenCapture->getImageLocation(); // --> /some/dir/test.png
+	echo $url;
+	die;
+
+} */
+
+/* function get_snapshot($host)
+{
+	$host = 'http://'.$host;	
+	
+	//call Google PageSpeed Insights API	
+	//$google_api_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=".$host."&key=AIzaSyCAQM8rZDg-LCt5hcVSlr75WFqJurK3kiE&screenshot=true";
+	$google_api_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=".$host."&filter_third_party_resources=true&locale=Malaysia&screenshot=true&snapshots=false&strategy=desktop&fields=screenshot";
+	//$googlePagespeedData = file_get_contents("https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=".$host."&screenshot=true");
+	//$googlePagespeedData = file_get_contents($google_api_url);
+
+	// 1. initialize
+	$ch = curl_init();
+	
+	$timeout = 100;
+	// 2. set the options, including the url
+	curl_setopt($ch, CURLOPT_URL, $google_api_url);	
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);	
+	
+	// 3. execute and fetch the resulting HTML output
+	$googlePagespeedData = curl_exec($ch);	
+	
+	// 4. free up the curl handle
+	curl_close($ch);
+	
+	//decode json data
+	$googlePagespeedData = json_decode($googlePagespeedData, true);	
+
+	//screenshot data
+	$screenshot = $googlePagespeedData['screenshot']['data'];	
+	$screenshot = str_replace(array('_','-'),array('/','+'),$screenshot);	
+	
+	return $screenshot;
+} */
+
+/* $array_site = array(
+					array("url" => 'sayaarief.blogspot.my', "title" => 'arief', "icon" => "fa fa-building"),
+					array("url" => 'amanz.my', "title" => 'amanz', "icon" => "fa fa-building"),
+					array("url" => 'kfzoom.blogspot.my', "title" => 'kfzoom', "icon" => "fa fa-building"),
+				); */
+
+$array_site = array(
+					array("pc"=>"http://", "url" => 'm3tech.com.my', "title" => 'M3 Tech', "icon" => "fa fa-building", "snapshot"=>0),
+					array("pc"=>"http://", "url" => 'm3asia.com', "title" => 'M3 Asia', "icon" => "fa fa-credit-card", "snapshot"=>0),
+					array("pc"=>"http://", "url" => 'm3online.com', "title" => 'M3 Online', "icon" => "fa fa-desktop", "snapshot"=>0),					
+					array("pc"=>"http://", "url" => 'getsnapps.com', "title" => 'GetSnapps', "icon" => "fa fa-android", "snapshot"=>0),
+					array("pc"=>"http://", "url" => 'apps.m3tech.asia', "title" => 'Apps M3 Tech', "icon" => "fa fa-apple", "snapshot"=>0),
+					array("pc"=>"http://", "url" => 'i3apps.com.my', "title" => 'i3 Apps', "icon" => "fa fa-mobile", "snapshot"=>0),
+					array("pc"=>"http://", "url" => 'support.m3asia.com', "title" => 'Support M3 Asia', "icon" => "fa fa-child", "snapshot"=>0),
+					array("pc"=>"http://", "url" => 'i3display.com', "title" => 'i3 Display', "icon" => "fa fa-play", "snapshot"=>0),					
+				);
+				
+$array_snapshots = array();
+/* foreach($array_site as $k=> $v)
+{
+	$check_snapshot = $v['snapshot'];
+	if($check_snapshot)
+	{
+		$array_snapshots[$v['url']] =  get_snapshot($v['url']);
+	}
+} */
+
+?>
+<title>Live Monitor</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="index.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<meta http-equiv="refresh" content="1800;url=index.php">
+<header class="rad-page-title"></header>
+<style type='text/css'>
+	.frame {
+    width: 100%;    
+    border: 0;
+	border-radius:5px;
+    -ms-transform: scale(1);
+    -moz-transform: scale(1);
+    -o-transform: scale(1);
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    
+    -ms-transform-origin: 0 0;
+    -moz-transform-origin: 0 0;
+    -o-transform-origin: 0 0;
+    -webkit-transform-origin: 0 0;
+    transform-origin: 0 0;
+}
+</style>
+<main style="padding-left:10px">
+<div class="row" style="color:black !important;">
+	<?php 
+		foreach($array_site as $k=> $v)
+		{ ?>			
+			<div class="col-lg-3 col-sm-6 col-xs-12">
+				<?php
+					//$x = check_status($v['url']);
+					$x = check_status_with_heroku($v['pc'],$v['url']);
+					$check_snapshot = $v['snapshot'];
+				?>
+				<div class="rad-info-box rad-txt-secondary" style="<?php echo $x; ?>">
+					<?php 
+						if($check_snapshot)
+						{
+							//$web_snapshot = get_snapshot($v['url']);
+							//$web_snapshot = $array_snapshots[$v['url']];
+							?>
+							<!--<img style="width:100%;border-radius:5px" src="data:image/jpeg;base64,<?php echo $web_snapshot; ?>"></img>-->
+						<?php
+						}
+						else
+						{?>
+							<i class="<?php echo $v['icon']; ?>"></i>
+						<?php
+						}
+						?>
+						
+					<br clear='all'/><br clear='all'/>
+					<span class="heading"><?php echo $v['url']; ?></span>
+					<span class="value"><span><?php echo $v['title']; ?></span></span>
+					<iframe class="frame" scrolling="no" style="border-radius:5px" width="100%" src="<?php echo $v['pc'].$v['url']; ?>">
+						<p>Your browser does not support iframes.</p>
+					</iframe>
+				</div>
+			</div>
+		<?php 
+		}
+	?>
+</div>
+</main>
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+		$('.heading').on('click',function()
+		{
+			var url = 'http://'+$(this).text();			
+			window.open(url, 'name'); 			
+		});
+	});
+</script>

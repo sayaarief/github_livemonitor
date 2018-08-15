049d4db8571e29b287af76ffd2a88d6cdf90b028
Arief Hilmi <arief.hilmi@gmail.com>
2018-08-15 09:41:32 +0800

    remove i3teamworks site

--- index.php
+++ index.php
@@ -196,10 +196,11 @@ $array_site = array(
 					array("pc"=>"http://", "url" => 'i3apps.com.my', "title" => 'i3 Apps', "icon" => "fa fa-mobile", "snapshot"=>0, "notify"=>1),
 					array("pc"=>"", "url" => 'support.m3asia.com', "title" => 'Support M3 Asia', "icon" => "fa fa-child", "snapshot"=>0, "notify"=>1),
 					array("pc"=>"www.", "url" => 'i3display.com', "title" => 'i3 Display', "icon" => "fa fa-play", "snapshot"=>0, "notify"=>0),
-					array("pc"=>"https://", "url" => 'm3.i3teamworks.com/login.php', "title" => 'M3 i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
-					array("pc"=>"https://", "url" => 'oa.i3teamworks.com/login.php', "title" => 'OA i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
-					array("pc"=>"https://", "url" => 'intl.i3teamworks.com/login.php', "title" => 'INTL i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
-					array("pc"=>"https://", "url" => 'cn.i3teamworks.cn/login.php', "title" => 'CN i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
+					array("pc"=>"www.", "url" => 'i3teamworks.com', "title" => 'i3 Teamworks', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
+					//array("pc"=>"https://", "url" => 'm3.i3teamworks.com/login.php', "title" => 'M3 i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
+					//array("pc"=>"https://", "url" => 'oa.i3teamworks.com/login.php', "title" => 'OA i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
+					//array("pc"=>"https://", "url" => 'intl.i3teamworks.com/login.php', "title" => 'INTL i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
+					//array("pc"=>"https://", "url" => 'cn.i3teamworks.cn/login.php', "title" => 'CN i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
 				);
 				
 $array_snapshots = array();

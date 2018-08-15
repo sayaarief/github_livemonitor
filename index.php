633807c8899bf2b1f5709f85c9f1b6b5f9f316d3
Arief Hilmi <arief.hilmi@gmail.com>
2018-08-15 15:08:16 +0800

    moments js

--- index.php
+++ index.php
@@ -219,6 +219,7 @@ $array_snapshots = array();
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="moments.js"></script>
+<script src="moment-timezone-with-data.js"></script>
 <link rel="stylesheet" type="text/css" href="index.css"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 <meta http-equiv="refresh" content="1800;url=index.php">

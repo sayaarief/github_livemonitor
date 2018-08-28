56107680a6ec052cb21adb48ac0dea169577c93b
Arief Hilmi <arief.hilmi@gmail.com>
2018-08-28 11:53:53 +0800

    update

--- index.php
+++ index.php
@@ -195,7 +195,7 @@ $array_site = array(
 					array("pc"=>"", "url" => 'apps.m3tech.asia', "title" => 'Apps M3 Tech', "icon" => "fa fa-apple", "snapshot"=>0, "notify"=>1),
 					array("pc"=>"http://", "url" => 'i3apps.com.my', "title" => 'i3 Apps', "icon" => "fa fa-mobile", "snapshot"=>0, "notify"=>1),
 					array("pc"=>"", "url" => 'support.m3asia.com', "title" => 'Support M3 Asia', "icon" => "fa fa-child", "snapshot"=>0, "notify"=>1),
-					array("pc"=>"www.", "url" => 'i3display.com', "title" => 'i3 Display', "icon" => "fa fa-play", "snapshot"=>0, "notify"=>0),					
+					array("pc"=>"www.", "url" => 'i3display.com', "title" => 'i3 Display', "icon" => "fa fa-play", "snapshot"=>0, "notify"=>1),					
 					array("pc"=>"https://", "url" => 'm3.i3teamworks.com/login.php', "title" => 'M3 i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
 					array("pc"=>"https://", "url" => 'oa.i3teamworks.com/login.php', "title" => 'OA i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),
 					array("pc"=>"https://", "url" => 'intl.i3teamworks.com/login.php', "title" => 'INTL i3tw', "icon" => "fa fa-universal-access", "snapshot"=>0, "notify"=>1),

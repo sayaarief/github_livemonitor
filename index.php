762f6a3b32398d6526b059b6a9ad7fcf3fb91a0d
Arief Hilmi <arief.hilmi@gmail.com>
2018-11-22 07:50:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ function check_status($http_code,$host)
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);	
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		//$result = curl_exec($ch);		
+		$result = curl_exec($ch);
 		curl_close($ch);
         //return $offline_style;
 		return -99;

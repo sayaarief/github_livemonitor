2d7223ffa24243a23158fccbdd1ebd258252f6ad
Arief Hilmi <arief.hilmi@gmail.com>
2020-01-29 15:15:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,8 +25,7 @@ function check_status_with_heroku($http_code,$host, $notify_flag)
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
 	curl_setopt($ch, CURLOPT_NOBODY  ,true);  // we don't need body
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch,  CURLOPT_USERAGENT,"page-check/1.0");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);	
     $data = curl_exec($ch);
 	if (curl_errno($ch))
 	{
